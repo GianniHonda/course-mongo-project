3899c08cbf4cbabad8c886c844195d31ad7ffe0a
GianniHonda <gianni.stephanie91@gmail.com>
2026-03-13 18:52:36 +0900

    Conecting to MongoDB with repository and service adjustments

--- src/main/java/com/giannihonda/workshopmongo/services/UserService.java
+++ src/main/java/com/giannihonda/workshopmongo/services/UserService.java
@@ -3,7 +3,6 @@ package com.giannihonda.workshopmongo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import com.giannihonda.workshopmongo.domain.User;
@@ -12,22 +11,10 @@ import com.giannihonda.workshopmongo.repository.UserRepository;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository repo;
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    public List<User> findAll() {
-        System.out.println("DATABASE = " + mongoTemplate.getDb().getName());
-        System.out.println("COLLECTIONS = " + mongoTemplate.getCollectionNames());
-
-        long count = mongoTemplate.getCollection("user").countDocuments();
-        System.out.println("COUNT user = " + count);
-
-        List<User> list = repo.findAll();
-        System.out.println("TOTAL USERS = " + list.size());
-        System.out.println(list);
-        return list;
-    }
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
 }
