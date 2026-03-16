7d65da09c8f68a82c5c30e75d179738f2d19e183
GianniHonda <gianni.stephanie91@gmail.com>
2026-03-16 16:12:18 +0900

    adding comments in the posts

--- src/main/java/com/giannihonda/workshopmongo/domain/Post.java
+++ src/main/java/com/giannihonda/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.giannihonda.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.giannihonda.workshopmongo.dto.AuthorDTO;
+import com.giannihonda.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String budy;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -63,18 +68,26 @@ public class Post implements Serializable {
 	public void setBudy(String budy) {
 		this.budy = budy;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	public AuthorDTO getAuthor() {
+	
+		public AuthorDTO getAuthor() {
 		return author;
 	}
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 
 	@Override
