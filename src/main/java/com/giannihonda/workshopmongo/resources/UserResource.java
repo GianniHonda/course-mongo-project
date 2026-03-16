0ac91323dd646bc4d8ac3820dddab19d6333cf09
GianniHonda <gianni.stephanie91@gmail.com>
2026-03-16 15:34:57 +0900

    endpoint for return an user posts

--- src/main/java/com/giannihonda/workshopmongo/resources/UserResource.java
+++ src/main/java/com/giannihonda/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.giannihonda.workshopmongo.domain.Post;
 import com.giannihonda.workshopmongo.domain.User;
 import com.giannihonda.workshopmongo.dto.UserDTO;
 import com.giannihonda.workshopmongo.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 	    obj.setId(id);
 	    obj = service.update(obj);
 	    return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
